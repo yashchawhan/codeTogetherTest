14e5df8adfbeb73d0504a33cde0338fd2e94a9c6
Ignacio Moreno <ignacio@codetogether.com>
2026-01-27 16:34:04 -0600

    05 todo crud operations (#12)
    * Read todos from file
    
    * todo crud operations

--- todo-json-backed/src/repository/TodoJsonRepository.ts
+++ todo-json-backed/src/repository/TodoJsonRepository.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs-extra'
 
 export interface ITodoDataSource {
     fetchTodos(): Promise<TodoDTO[]>
+    saveTodos(todos: TodoDTO[]): Promise<void>
 }
 
 export class TodoDataJsonSourceImpl implements ITodoDataSource {
@@ -13,6 +14,15 @@ export class TodoDataJsonSourceImpl implements ITodoDataSource {
 
     constructor(private basePath: string = path.join(__dirname, '../../', 'data')) {
         // do nothing
+    }
+
+    async saveTodos(todos: TodoDTO[]): Promise<void> {
+        const fullPath = path.join(this.basePath, TodoDataJsonSourceImpl.FILE_PATH)
+        try {
+            await fs.writeFile(fullPath, JSON.stringify(todos, null, 2), 'utf-8')
+        } catch (error) {
+            console.error(`Error writing todos to file: ${error}`)
+        }
     }
 
     public async fetchTodos():  Promise<TodoDTO[]> {
@@ -33,6 +43,35 @@ export class TodoRepositoryImpl implements ITodoRepository {
 
     constructor(private dataSource: ITodoDataSource) {
         // do nothing
+    }
+
+    async markTodoAsCompleted(id: string): Promise<void> {
+        const todos = await this.getTodos()
+        const todo = todos.find(t => t.id === id)
+        if (todo) {
+            todo.completed = true
+            await this.dataSource.saveTodos(todos)
+        }
+    }
+
+    async addTodo(title: string): Promise<TodoDTO> {
+        const todos = await this.getTodos()
+        const last = todos[todos.length - 1]
+        const nextId = '' + (last ? Number(last.id) + 1 : 1)
+        const newTodo: TodoDTO = {
+            id: nextId,
+            title: title,
+            completed: false
+        }
+        todos.push(newTodo)
+        await this.dataSource.saveTodos(todos)
+        return newTodo
+    }
+
+    async removeTodo(id: string): Promise<void> {
+        const todos = await this.getTodos()
+        const filteredTodos = todos.filter(t => t.id !== '' + id)
+        await this.dataSource.saveTodos(filteredTodos)
     }
 
     async getTodos():  Promise<TodoDTO[]> {
